ae21d2e06378f86a72ebebf727fbd6f3a31e83c3
edwinmwai20 <musyokaedwin01@gmail.com>
2026-03-01 23:01:01 +0300

    Completed whole (#7)

--- gym/services.py
+++ gym/services.py
@@ -1,1 +1,44 @@
-#service.py
+# gym/services.py
+
+from gym.storage import load_members, save_members
+
+
+def add_member():
+    members = load_members()
+
+    name = input("Member name: ")
+    plan = input("Plan: ")
+
+    new_id = len(members) + 1
+
+    new_member = {
+        "id": new_id,
+        "name": name,
+        "plan": plan,
+        "status": "Active"
+    }
+
+    members.append(new_member)
+    save_members(members)
+
+    print("Member added.")
+
+
+def view_members():
+    members = load_members()
+
+    for m in members:
+        print(m)
+
+
+def deactivate_member():
+    members = load_members()
+
+    member_id = int(input("Member ID: "))
+
+    for m in members:
+        if m["id"] == member_id:
+            m["status"] = "Inactive"
+
+    save_members(members)
+    print("Member deactivated.")
